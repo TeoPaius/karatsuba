9ea83aeb14766bf06beef37fd279a7c5adab5e6d
Teo <teodor_paius@yahoo.com>
2018-11-22 17:14:53 +0200

    project up, testing kraken

---
+++ cmake-build-debug/CMakeFiles/karatsuba.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/karatsuba.dir/main.cpp.obj"
+  "karatsuba.pdb"
+  "karatsuba.exe"
+  "karatsuba.exe.manifest"
+  "libkaratsuba.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/karatsuba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
